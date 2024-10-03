8ba9d6f3a554aef545d32ffdf5479c356fb04752
Ram Kumar Dhimal <ramkumardhimal2058@gmail.com>
2024-10-04 00:51:54 +1000

    first

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log("Hello World");
+
+function bulbOn (){
+    document.getElementById("bulb").src = "./bulb-on.png";
+}
+
+function bulbOff(){
+    document.getElementById("bulb").src= "./bulb-off.png";
+}
